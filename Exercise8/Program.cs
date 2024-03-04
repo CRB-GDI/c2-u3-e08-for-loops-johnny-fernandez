2898b1c1f5080e569a7fbeccd598d090d28f229f
johnny-fernandez <145487736+johnny-fernandez@users.noreply.github.com>
2024-03-04 13:07:03 -0500

    exercise8 completed

---
+++ Exercise8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Transactions;
+
+namespace Exercise8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a max number: ");
+            int maxNum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < maxNum; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine("Enter an amount of asterisks: ");
+            int maxAst = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < maxAst; i++)
+            {
+            
+                
+                    Console.Write("*");
+                
+
+            }
+            //Console.WriteLine(maxNum);
+            //asterisk
+        }
+    }
+}
